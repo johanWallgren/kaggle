c320fb08aa88f6401677415f217d62e0532e4d4e
Johan Wållgren <johan.g.wallgren@gmail.com>
2018-10-10 12:34:05 +0200

    Update pubgXGBoost.R

--- pubg/pubgXGBoost.R
+++ pubg/pubgXGBoost.R
@@ -8,6 +8,10 @@ library(Matrix)
 train <- as_tibble(fread('train.csv', sep = ','))
 test <- as_tibble(fread('test.csv', sep = ','))
 
+# To run in kernel on kaggle
+# train <- as_tibble(fread('../input/train.csv', sep = ','))
+# test <- as_tibble(fread('../input/test.csv', sep = ','))
+
 ########################
 
 # Remove matches with large groups 
@@ -22,64 +26,73 @@ nPlayersInGroup <- group_by(test, matchId) %>%
   group_by(groupId) %>%
   summarize(nPlayersInGroup = n())
 
+test <- left_join(test, nPlayersInGroup, by = 'groupId')
+
 maxPlayersInGroupTest <- max(nPlayersInGroup$nPlayersInGroup)
 
 largeTrainGroups <- filter(train, nPlayersInGroup > maxPlayersInGroupTest)
 
-train <- filter(train, matchId != any(unique(largeTrainGroups$matchId))) %>%
-  select(-nPlayersInGroup)
+train <- filter(train, matchId != any(unique(largeTrainGroups$matchId)))
+
 
 ########################
-# Removing matches where max(winPlacePerc) is less than 1
-
+# Removing matches from training data where maxWinSpread is less than 1
 maxWinSpread <- group_by(train, matchId) %>%
   summarize(maxWinSpread = max(winPlacePerc) - min(winPlacePerc))
+
 
 train <- left_join(train, maxWinSpread, by = 'matchId') %>%
   filter(maxWinSpread == 1) %>%
   select(-maxWinSpread)
 
 ########################
+# Feature engineering
 
-playersInMatchTest <- group_by(test, matchId) %>%
+# Adding totalDist = rideDistance + swimDistance + walkDistance
+# Adding healsAndBoosts = heals + boosts
+# Normalizing features that depend on number of players in match
+
+nPlayersInMatch <- group_by(train, matchId) %>%
   summarize(nPlayersInMatch = n())
 
-hist(playersInMatchTest$nPlayersInMatch,100)
+train <- left_join(train, nPlayersInMatch, by = 'matchId')
 
-playersInMatchTrain <- group_by(train, matchId) %>%
+train <- mutate(train, totalDist = rideDistance + swimDistance + walkDistance) %>%
+  mutate(healsAndBoosts = heals + boosts) %>%
+  mutate(kills = kills * 100 / nPlayersInMatch)
+
+
+nPlayersInMatch <- group_by(test, matchId) %>%
   summarize(nPlayersInMatch = n())
 
-hist(playersInMatchTrain$nPlayersInMatch,100)
+test <- left_join(test, nPlayersInMatch, by = 'matchId')
+
+test <- mutate(test, totalDist = rideDistance + swimDistance + walkDistance) %>%
+  mutate(healsAndBoosts = heals + boosts)%>%
+  mutate(kills = kills * 100 / nPlayersInMatch)
 
 ########################
 # Drop group data and Id's
-train <- select(train, -numGroups, -maxPlace, -matchId, -groupId, -Id)
+train <- select(train, -numGroups, -maxPlace, -matchId, -groupId, -Id, -nPlayersInMatch, -nPlayersInGroup)
 
 testID <- test$Id
 testMatchID <- test$matchId
-test <- select(test, -numGroups, -maxPlace, -matchId, -groupId, -Id)
+test <- select(test, -numGroups, -maxPlace, -matchId, -groupId, -Id, -nPlayersInMatch, -nPlayersInGroup)
 
 ########################
-# XGBoost
-
+# Prepping for training 
 winPlacePercColumn <- which(names(train) == 'winPlacePerc')
+
 y <- train[ ,winPlacePercColumn]
 
-# Deviding data in to test and train
-smp_size <- floor(0.95 * nrow(train))
-train_ind <- sample(seq_len(nrow(train)), size = smp_size, replace = FALSE)
-
-Xtrain <- train[train_ind, ]
-Xtest <- train[-train_ind, ]
-ytrain <- y[train_ind, ]
-ytest <- y[-train_ind, ]
+########################
 
 tic()
-xgb <- xgboost(data = data.matrix(Xtrain[-winPlacePercColumn]),
-               label = data.matrix(ytrain),
+xgb <- xgboost(data = data.matrix(train[-winPlacePercColumn]), 
+               label = data.matrix(y),
                booster = 'gbtree',
                eta = 0.2, # step size of each boosting step
-               nround = 10,
+               nround = 250,
                eval_metric = 'rmse',
                objective = 'reg:linear',
                tree_method = 'exact',
@@ -88,19 +101,12 @@ xgb <- xgboost(data = data.matrix(Xtrain[-winPlacePercColumn]),
                colsample_bytree = 0.8)
 toc()
 
-y_pred <- as_tibble(predict(xgb, data.matrix(Xtest[-winPlacePercColumn]))) %>%
-  mutate(value = ifelse(value > 1, 1, value),
-         value = ifelse(value < 0, 0, value))
-
-
-# Using root mean squared as error function
-rmseXGB <- sqrt(sum((y_pred - ytest)^2) / nrow(ytest))
-print(rmseXGB)
-
+########################
 # Predicting on test
 test_pred <- predict(xgb, data.matrix(test))
 
 
+########################
 # All matches should have winPlacePerc with range 0-1
 test <- bind_cols(as_tibble(testID), as_tibble(testMatchID), test, as_tibble(test_pred)) %>%
   rename(Id = value, matchId = value1, winPlacePerc = value2)
@@ -108,21 +114,20 @@ test <- bind_cols(as_tibble(testID), as_tibble(testMatchID), test, as_tibble(tes
 maxWinSpreadTest <- group_by(test, matchId) %>%
   summarize(maxWinSpread = max(winPlacePerc) - min(winPlacePerc))
 
-hist(maxWinSpreadTest$maxWinSpread,100)
-
 # maxWinSpreadTest sholud be equal to 1 for test, use normailize to fix
-
 maxWinTest <- group_by(test, matchId) %>%
   summarize(maxWin = max(winPlacePerc))
-
 test <- left_join(test, maxWinTest, by = "matchId")
 
 minWinTest <- group_by(test, matchId) %>%
   summarize(minWin = min(winPlacePerc))
-
 test <- left_join(test, minWinTest, by = "matchId")
 
-test <- mutate(test, winPlacePercNorm = (winPlacePerc - minWin) / (maxWin - minWin))
+test <- mutate(test, winPlacePercNorm = (winPlacePerc - minWin) / (maxWin - minWin)) %>%
+  mutate(winPlacePercNorm = ifelse(is.na(winPlacePercNorm) == 1, 0, winPlacePercNorm))
 
+########################
 submission <- bind_cols(as_tibble(testID), as_tibble(test$winPlacePercNorm)) %>%
   rename(Id = value, winPlacePerc = value1)
+
+write.csv(submission, file = "submission.csv",row.names=FALSE, quote = FALSE)
